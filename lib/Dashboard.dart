aa8ea95e488ef5d8a6deab3e428adaadab5adcd3
Milzam Hibatullah <hmilzam@gmail.com>
2020-08-02 14:06:22 +0700

    succes to get link

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uni_links/uni_links.dart';
 
-class Dashboard extends StatefulWidget{
+class Dashboard extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return DashboardState();
   }
 }
 
-class DashboardState extends State<Dashboard>{
+class DashboardState extends State<Dashboard> {
+  @override
+  void initState() {
+    _initUniLinks();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Text('Hello World'),
+        child: Text('Hello World  '),
       ),
     );
-
   }
+
+  _initUniLinks()async{
+    try{
+      Future<String> initialLink = getInitialLink();
+      initialLink.then((value) => print('linknya adalah : '+value.toString()));
+    }catch(e){
+      print(e);
+    }
+  }
+
 }
